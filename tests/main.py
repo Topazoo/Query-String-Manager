94c43f507e6642fc1580c7b587575d08eb7f4857
Peter Swanson <topazoo42@gmail.com>
2022-11-27 13:05:04 -0800

    [Tests] Update suite

--- tests/main.py
+++ tests/main.py
@@ -3,5 +3,6 @@ import unittest
 
 from .utilities_tests.test_is_valid_single_level_dict import *
 from .encoders_tests.test_generate_query_string import *
+from .encoders_tests.test_generate_base64_query_string import *
 
 unittest.main(argv=sys.argv)
