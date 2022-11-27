5f9819943a096c970253abf547fb2a589856236c
Peter Swanson <topazoo42@gmail.com>
2022-11-26 17:26:59 -0800

    [Tests] Add tests for _generate_raw_query_string()

--- tests/encoders_tests/test_generate_raw_query_string.py
+++ tests/encoders_tests/test_generate_raw_query_string.py
@@ -9,11 +9,20 @@ class TestGenerateRawQueryString(unittest.TestCase):
 
     def test_throws_exception_on_invalid_dict(self):
         """
-        This method should throw a ValueError if the passed dictionary of params is not single level
-        or does not have values that can be written to a query string
+        This method should throw a ValueError if the passed dictionary of params is not a
+        valud "single level" dictionary or does not have values that can be written to a 
+        query string
         """
 
-        pass
+        TEST_INVALID_DICTS = [
+            None,
+            1,
+            {"test": {}},
+            {"test": None}
+        ]
+
+        for test_dict in TEST_INVALID_DICTS:
+            self.assertRaises(ValueError, QueryStringParser._generate_raw_query_string(test_dict))
 
 
     def test_creates_single_arg_query_string(self):
@@ -24,7 +33,16 @@ class TestGenerateRawQueryString(unittest.TestCase):
         "?key=value"
         """
 
-        pass
+        TEST_DICTS_AND_RESULTS = [
+            ({"key": "value"}, "?key=value"),
+            ({"test": 1}, "?test=1"),
+            ({"test": True}, "?test=true"),
+            ({"test": False}, "?test=false"),
+            ({"test": 3.14}, "?test=3.14"),
+        ]
+
+        for test_dict in TEST_DICTS_AND_RESULTS:
+            self.assertEquals(test_dict[1], QueryStringParser._generate_raw_query_string(test_dict[0]))
 
 
     def test_creates_multiple_arg_query_string(self):
@@ -36,7 +54,13 @@ class TestGenerateRawQueryString(unittest.TestCase):
         "?key=value&key2=value2"
         """
 
-        pass
+        TEST_DICTS_AND_RESULTS = [
+            ({"key": "value", "key2": "value2"}, "?key=value&key2=value2"),
+            ({"test": 1, "test2": 3.14}, "?test=1&test2=3.14"),
+            ({"test": True, "val2": False}, "?test=true&val2=false"),
+            ({"test": -1, "test2": "hello", "test3": False, "test4": 0.01}, "?test=-1&test2=hello&test3=false&test4=0.01"),
+        ]
 
-
+        for test_dict in TEST_DICTS_AND_RESULTS:
+            self.assertEquals(test_dict[1], QueryStringParser._generate_raw_query_string(test_dict[0]))
     
